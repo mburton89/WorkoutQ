796ccc4ecbd8d47a80dd193b4465a1eb8f6174c2
mburton89 <matthewwburton@gmail.com>
2018-08-29 22:01:18 -0500

    Made PanelMover class and prefab that handles moving panle's order in menu and also handles deleting panels

---
+++ Workout Q/Assets/Scripts/PanelMover.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelMover : MonoBehaviour 
+{
+	private UIPanel _panel;
+	private GridLayoutGroup _activeGridLayoutGroup;
+
+	[SerializeField]private Button moveUpButton;
+	[SerializeField]private Button deleteButton;
+	[SerializeField]private Button moveDownButton;
+
+	void Start()
+	{
+		_panel = GetComponentInParent<UIPanel>();
+		_activeGridLayoutGroup = GetComponentInParent<GridLayoutGroup>();
+	}
+
+	void OnEnable()
+	{
+		moveUpButton.onClick.AddListener(MovePanelUp);
+		deleteButton.onClick.AddListener(DeletePanel);
+		moveDownButton.onClick.AddListener(MovePanelDown);
+	}
+
+	void MovePanelUp()
+	{
+		int siblingIndex = _panel.transform.GetSiblingIndex();
+
+		if(siblingIndex > 0)
+		{
+			_panel.transform.SetSiblingIndex(siblingIndex - 1);
+		}
+		WorkoutManager.Instance.Save();
+	}
+
+	void DeletePanel()
+	{
+		Destroy(_panel.gameObject);
+		WorkoutManager.Instance.Save();
+	}
+
+	void MovePanelDown()
+	{
+		int siblingIndex = _panel.transform.GetSiblingIndex();
+		int childrenCount = _activeGridLayoutGroup.transform.childCount;
+
+		if(siblingIndex < childrenCount)
+		{
+			_panel.transform.SetSiblingIndex(siblingIndex + 1);
+		}
+		WorkoutManager.Instance.Save();
+	}
+}
