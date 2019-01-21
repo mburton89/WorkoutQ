626cc96894dfb8dd17e7a6233bd5b63711e64c05
DESKTOP-AVTI26A\burto <matthewwburton@gmail.com>
2019-01-20 19:15:05 -0600

    The rest

---
+++ Workout Q/Assets/Scripts/WorkoutGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkoutGenerator : MonoBehaviour {
+
+	public static WorkoutGenerator Instance;
+
+	public WorkoutData ExampleChestTricep;
+	public WorkoutData ExampleBackBicep;
+	public WorkoutData ExampleLegs;
+	public WorkoutData ExampleShoulders;
+	public WorkoutData ExampleCore;	
+
+	void Awake()
+	{
+		Instance = this;
+	}
+}
