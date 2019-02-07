948e5bb774ccca9ad372c9f80dc52007b0791c61
DESKTOP-AVTI26A\burto <matthewwburton@gmail.com>
2019-02-06 21:55:15 -0600

    Added scripts

---
+++ Workout Q/Assets/Scripts/Config/WeightTypeConfigurer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeightTypeConfigurer : MonoBehaviour {
+
+	public Slider weightTypeSlider;
+
+	void Start()
+	{
+		if (PlayerPrefs.GetString ("weightType") == "kg") 
+		{
+			weightTypeSlider.value = 1;	
+		} 
+	}
+
+	void OnEnable()
+	{
+		weightTypeSlider.onValueChanged.AddListener (HandleWeightTypeChanged);
+	}
+
+	void OnDisable()
+	{
+		weightTypeSlider.onValueChanged.RemoveListener(HandleWeightTypeChanged);
+	}
+
+	void HandleWeightTypeChanged(float weightType)
+	{
+		if (weightType == 0) {
+			PlayerPrefs.SetString ("weightType", "lb");
+		} else {
+			PlayerPrefs.SetString ("weightType", "kg");		
+		}
+	}
+}
