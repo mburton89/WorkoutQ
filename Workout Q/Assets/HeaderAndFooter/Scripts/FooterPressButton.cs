2406e4cacdeac345c5d88467a95f248c3e4abaed
DESKTOP-AVTI26A\burto <matthewwburton@gmail.com>
2019-05-13 23:11:04 -0500

    Missed stuff

---
+++ Workout Q/Assets/HeaderAndFooter/Scripts/FooterPressButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class FooterPressButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+	private RectTransform _rect;
+	private Vector2 _initialPosition;
+	private float _amountToPress = 10f;
+
+	void Awake()
+	{
+		_rect = GetComponent<RectTransform>();
+		_initialPosition = _rect.anchoredPosition;
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		_rect.anchoredPosition = new Vector2(_initialPosition.x, _initialPosition.y - _amountToPress);
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		_rect.anchoredPosition = _initialPosition;
+	}
+}
