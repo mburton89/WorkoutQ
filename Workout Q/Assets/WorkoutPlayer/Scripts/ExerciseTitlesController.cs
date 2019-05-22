bb80656c25f431a942e6dc8e761359cb3c929514
Matthew Burton <matthew.burton@projectpixelpress.com>
2019-05-22 08:36:02 -0500

    scdc

--- Workout Q/Assets/WorkoutPlayer/Scripts/ExerciseTitlesController.cs
+++ Workout Q/Assets/WorkoutPlayer/Scripts/ExerciseTitlesController.cs
@@ -3,33 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class ExerciseTitlesController : MonoBehaviour {
+public class ExerciseTitlesController : MonoBehaviour
+{
+    private WorkoutPlayerController _controller;
 
-	private WorkoutPlayerController _controller;
+    [SerializeField] private TextMeshProUGUI _activeExerciseTitle;
+    [SerializeField] private TextMeshProUGUI _nextExerciseTitle;
 
-	[SerializeField] private TextMeshProUGUI _activeExerciseTitle;
-	[SerializeField] private TextMeshProUGUI _nextExerciseTitle;
-	//[SerializeField] private TextMeshProUGUI _nextExerciseLabel;
+    public void Init(WorkoutPlayerController controller)
+    {
+        _controller = controller;
+        _activeExerciseTitle.color = ColorManager.Instance.ActiveColorLight;
+        _nextExerciseTitle.color = ColorManager.Instance.ActiveColorDark;
+    }
 
-	public void Init(WorkoutPlayerController controller)
-	{
-		_controller = controller;
-		_activeExerciseTitle.color = ColorManager.Instance.ActiveColorLight;
-		_nextExerciseTitle.color = ColorManager.Instance.ActiveColorLight;
-	}
+    public void UpdateActiveExerciseTitle(string currentExerciseTitle)
+    {
+        _activeExerciseTitle.text = currentExerciseTitle;
+    }
 
-	public void UpdateActiveExerciseTitle(string currentExerciseTitle)
-	{
-		_activeExerciseTitle.text = currentExerciseTitle;
-	}
+    public void UpdateNextExerciseTitle(string nextExerciseTitle, int weightValue)
+    {
+        _nextExerciseTitle.text = "Next: " + nextExerciseTitle + " " + weightValue + PlayerPrefs.GetString("weightType") + "s";
+        //_nextExerciseLabel.text = "Next";
+    }
 
-	public void UpdateNextExerciseTitle(string nextExerciseTitle, int weightValue)
-	{
-		_nextExerciseTitle.text = nextExerciseTitle + " " + weightValue + PlayerPrefs.GetString ("weightType") + "s";
-	}
-
-	public void HideNextExerciseText()
-	{
-		_nextExerciseTitle.text = string.Empty;
-	}
+    public void HideNextExerciseText()
+    {
+        _nextExerciseTitle.text = string.Empty;
+        //_nextExerciseLabel.text = string.Empty;
+    }
 }
